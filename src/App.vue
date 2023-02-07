a87f9860d0cb892ebc68460f55ff96d5591fb8d7
rodhis <rodrigonoliva@gmail.com>
2023-02-07 20:20:00 -0300

    structure

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="px-3 py-10 md:px-10">
+    <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
+      <TodoSpinner />
+
+      <TodoFormAdd />
+
+      <TodoItens />
+
+      <TodoEmpty />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoSpinner from "./components/TodoSpinner.vue";
+import TodoFormAdd from "./components/TodoFormAdd.vue";
+import TodoItens from "./components/TodoItens.vue";
+import TodoEmpty from "./components/TodoEmpty.vue";
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+
+  components: { TodoSpinner, TodoFormAdd, TodoItens, TodoEmpty },
+
+  data() {
+    return {
+    todos: []
+    }
+  },
+
+  created() {
+    axios.get("http://localhost:3000/todos")
+    .then((response) =>  {
+      this.todos = response.data
+    })
+  },
+};
 </script>
 
 <style>
